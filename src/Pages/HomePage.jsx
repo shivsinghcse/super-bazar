e1d44378c78771d447ce05b111a09fe36e4c125a
Shiv Singh <shivsinghcse@gmail.com>
2025-08-03 09:59:51 +0530

    fetch data from backend

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,16 @@
-import "./HomePage.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
-import { products } from "../../starting-code/data/products";
+import "./HomePage.css";
+
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <>
       <title>Super Bazar</title>
@@ -9,55 +18,6 @@ const HomePage = () => {
 
       <div className="home-page">
         <div className="products-grid">
-          <div className="product-container">
-            <div className="product-image-container">
-              <img
-                className="product-image"
-                src="images/products/athletic-cotton-socks-6-pairs.jpg"
-              />
-            </div>
-
-            <div className="product-name limit-text-to-2-lines">
-              Black and Gray Athletic Cotton Socks - 6 Pairs
-            </div>
-
-            <div className="product-rating-container">
-              <img
-                className="product-rating-stars"
-                src="images/ratings/rating-45.png"
-              />
-              <div className="product-rating-count link-primary">87</div>
-            </div>
-
-            <div className="product-price">$10.90</div>
-
-            <div className="product-quantity-container">
-              <select>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-
-            <div className="product-spacer"></div>
-
-            <div className="added-to-cart">
-              <img src="images/icons/checkmark.png" />
-              Added
-            </div>
-
-            <button className="add-to-cart-button button-primary">
-              Add to Cart
-            </button>
-          </div>
-
           {products.map((product) => {
             return (
               <div className="product-container" key={product.id}>
@@ -72,14 +32,18 @@ const HomePage = () => {
                 <div className="product-rating-container">
                   <img
                     className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars * 10}.png`}
+                    src={`images/ratings/rating-${
+                      product.rating.stars * 10
+                    }.png`}
                   />
                   <div className="product-rating-count link-primary">
                     {product.rating.count}
                   </div>
                 </div>
 
-                <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">
+                  ${(product.priceCents / 100).toFixed(2)}
+                </div>
 
                 <div className="product-quantity-container">
                   <select>
